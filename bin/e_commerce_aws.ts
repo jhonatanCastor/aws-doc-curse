d4a14a9a833fc0fd01160bda029e80cd6f11bfd5
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-03-22 14:25:41 -0300

    first commit

---
+++ bin/e_commerce_aws.ts
@@ -0,0 +1,35 @@
+import * as cdk from 'aws-cdk-lib';
+import { ProductsAppStack } from '../lib/productsApp-stack';
+import { ECommerceAPIStack } from '../lib/e_commerce_aws-stack';
+import { ProductsAppLayersStack } from '../lib/productsAppLayers-stack';
+
+const app = new cdk.App();
+
+const env: cdk.Environment = {
+  account: "975050107385",
+  region: "us-east-1"
+};
+
+const tags = {
+  cost: "ECommerce",
+  team: "SiecolaCode"
+};
+
+const productsAppLayersStack = new ProductsAppLayersStack(app, "ProductsAppLayers", {
+  tags: tags,
+  env: env
+});
+
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
+  tags: tags,
+  env: env
+});
+productsAppStack.addDependency(productsAppLayersStack);
+
+const eCommerceApiStack = new ECommerceAPIStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  productsAdminHandler: productsAppStack.productAdminHandler,
+  tags: tags,
+  env: env
+});
+eCommerceApiStack.addDependency(productsAppStack);
